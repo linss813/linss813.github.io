a3d31d65d2699cb13161cf7e2a74b8320ea646f1
linss <lin813ss@163.com>
2026-01-08 16:57:22 +0800

    20260108-4-test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,16 +1,19 @@
 import { defineUserConfig } from "vuepress";
-
+import { getDirname, path } from "@vuepress/utils"; // 必须引入此工具
 import theme from "./theme.js";
 
+const __dirname = getDirname(import.meta.url);
+
 export default defineUserConfig({
-  base: "/",
+  base: "/", // 你的域名是根路径，此处保持 "/"
 
   lang: "zh-CN",
   title: "十三月",
   description: "linss813",
 
-  theme,
+  // 核心修改：将公共资源目录指向 src/public（不再是隐藏文件夹）
+  // 这样 Obsidian 就能直接识别并选择它了
+  public: path.resolve(__dirname, "../public"), 
 
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
+  theme,
 });
